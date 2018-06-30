0d3a0bf4b7c72764f64dd5d4766b507ea3627453
Peng Guanwen <pg999w@outlook.com>
2018-06-30 23:26:08 +0800

    添加 EF Migration

---
+++ Migrations/20180630151434_Migration2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Syzoj.Api.Migrations
+{
+    public partial class Migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+        }
+    }
+}
