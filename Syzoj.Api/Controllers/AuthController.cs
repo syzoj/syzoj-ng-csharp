aeb060693b8fb74f714af006d71bb84b79f3ec4a
t123yh <t123yh@outlook.com>
2018-07-01 11:22:34 +0800

    Add sln.

---
+++ Syzoj.Api/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Syzoj.Api.Data;
+using Syzoj.Api.Filters;
+using Syzoj.Api.Models;
+
+namespace Syzoj.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly IDistributedCache cache;
+
+        public AuthController(ApplicationDbContext dbContext, IDistributedCache cache)
+        {
+            this.dbContext = dbContext;
+            this.cache = cache;
+        }
+
+        // POST /api/auth/login
+        [HttpPost]
+        public string Login(string userName, string password)
+        {
+            return $"{userName} Login!";
+        }
+
+        // POST /api/auth/logout
+        [HttpPost]
+        public string Logout()
+        {
+            return "Logout!";
+        }
+
+        // POST /api/auth/register
+        [RecaptchaValidation]
+        [HttpPost]
+        public async Task<JsonResult> Register([FromForm]RegisterApiModel addUser)
+        {
+            var (salt, hash) = Utils.HashUtils.GenerateHashedPassword(addUser.Password);
+            User user = new User
+            {
+                Name = addUser.Name,
+                Email = addUser.Email,
+                HashedPassword = Convert.ToBase64String(hash),
+                PasswordSalt = Convert.ToBase64String(salt)
+            };
+            dbContext.Users.Add(user);
+            await dbContext.SaveChangesAsync();
+            return new JsonResult(new {
+                status = 0,
+                user = new { name = user.Name, email = user.Email },
+            });
+        }
+
+    }
+}
---
+++ Syzoj.Api/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Syzoj.Api.Data;
+
+namespace Syzoj.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            // 使用 Redis
+            services.AddDistributedRedisCache(options =>
+            {
+                // TODO: 究竟应该用什么配置？
+                options.Configuration = "localhost";
+                options.InstanceName = "Syzoj";
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            // 应用不应该使用 HTTPS
+            // app.UseHttpsRedirection();
+
+            app.UseMvc();
+        }
+    }
+}
