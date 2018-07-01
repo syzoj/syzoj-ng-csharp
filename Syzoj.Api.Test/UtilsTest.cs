c332b7d42259493eaa0c0e6f4fd187b6c495aec4
Peng Guanwen <pg999w@outlook.com>
2018-07-01 23:03:59 +0800

    [TEST] Add integration  test

---
+++ Syzoj.Api.Test/UtilsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Syzoj.Api.Utils;
+using Xunit;
+
+namespace Syzoj.Api.Test
+{
+    public class UtilsTest
+    {
+        [Fact]
+        public void AwaysTure()
+        {
+            Assert.Equal(5, 5);
+        }
+
+        [Theory]
+        [InlineData("1232`423434")]
+        [InlineData("salfdhaf;lash")]
+        public void PasswordIsSame(string password)
+        {
+            var (salt, hash) = HashUtils.GenerateHashedPassword(password);
+
+            Assert.True(HashUtils.VerifyHash(salt, hash, password));
+        }
+    }
+}
