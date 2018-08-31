d64494585177ba8bf69853bb25e482f71c667a97
hewenyang <hewenyang@hewenyang.cn>
2018-08-31 15:41:47 +0800

    Implement provider services

--- Syzoj.Api/Services/ProblemResolverProvider.cs
+++ Syzoj.Api/Services/ProblemResolverProvider.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Syzoj.Api.Controllers;
 using Syzoj.Api.Models.Data;
 
@@ -5,9 +7,19 @@ namespace Syzoj.Api.Services
 {
     public class ProblemResolverProvider : IProblemResolverProvider
     {
+        private static IDictionary<ProblemType, Type> ProblemResolvers = new Dictionary<ProblemType, Type>()
+        {
+
+        };
+
+        private readonly IServiceProvider serviceProvider;
+        public ProblemResolverProvider(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
         public IProblemResolver GetProblemResolver(Problem problem)
         {
-            throw new System.NotImplementedException();
+            return (IProblemResolver) serviceProvider.GetService(ProblemResolvers[problem.Type]);
         }
     }
 }
--- Syzoj.Api/Services/ProblemSetServiceProvider.cs
+++ Syzoj.Api/Services/ProblemSetServiceProvider.cs
@@ -1,12 +1,24 @@
+using System;
+using System.Collections.Generic;
 using Syzoj.Api.Models.Data;
 
 namespace Syzoj.Api.Services
 {
     public class ProblemSetServiceProvider : IProblemSetServiceProvider
     {
+        private static IDictionary<ProblemSetType, Type> ProblemSetServices = new Dictionary<ProblemSetType, Type>()
+        {
+
+        };
+
+        private readonly IServiceProvider serviceProvider;
+        public ProblemSetServiceProvider(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
         public IProblemSetService GetProblemSetService(ProblemSet problemSet)
         {
-            throw new System.NotImplementedException();
+            return (IProblemSetService) serviceProvider.GetService(ProblemSetServices[problemSet.Type]);
         }
     }
 }
