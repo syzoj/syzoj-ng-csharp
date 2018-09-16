539df88260ef5e4b9e6b4156ae2191049274f598
hewenyang <hewenyang@hewenyang.cn>
2018-09-16 14:41:29 +0800

    Implement LegacySyzojProblemParser.ParseProblemAsync

--- Syzoj.Api/Services/LegacySyzojProblemParser.cs
+++ Syzoj.Api/Services/LegacySyzojProblemParser.cs
@@ -1,6 +1,10 @@
+using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using SharpFileSystem;
+using Syzoj.Api.Data;
 using Syzoj.Api.Models;
+using MessagePack;
 
 namespace Syzoj.Api.Services
 {
@@ -17,12 +21,14 @@ namespace Syzoj.Api.Services
     /// The `data` folder contains all testdata files.
     /// </summary>
 
-    public class LegacySyzojProblemParser : IAsyncProblemParser
+    public sealed class LegacySyzojProblemParser : IAsyncProblemParser
     {
         private readonly IFileSystem fileSystem;
-        public LegacySyzojProblemParser(IFileSystem fileSystem)
+        private readonly ApplicationDbContext dbContext;
+        public LegacySyzojProblemParser(IFileSystem fileSystem, ApplicationDbContext dbContext)
         {
             this.fileSystem = fileSystem;
+            this.dbContext = dbContext;
         }
 
         public Task HandleSubmissionAsync(Problem problem, Submission submission, object data)
@@ -35,9 +41,60 @@ namespace Syzoj.Api.Services
             return Task.Run(() => fileSystem.Exists(FileSystemPath.Parse(problem.Path).AppendFile("syzoj-export.json")));
         }
 
-        public Task ParseProblemAsync(Problem problem)
+        public async Task ParseProblemAsync(Problem problem)
         {
-            throw new System.NotImplementedException();
+            var obj = await Task.Run(() => {
+                var stream = fileSystem.OpenFile(FileSystemPath.Parse(problem.Path).AppendFile("syzoj-export.json"), FileAccess.Read);
+                var serializer = new JsonSerializer();
+                using(var reader = new StreamReader(stream)) {
+                    using(var jsonReader = new JsonTextReader(reader)) {
+                        return serializer.Deserialize<ExportJson>(jsonReader);
+                    }
+                }
+            });
+            if(obj.success)
+            {
+                problem.Title = obj.obj.title;
+                problem.Statement = MessagePackSerializer.Serialize(new {
+                    Description = obj.obj.description,
+                    InputFormat = obj.obj.input_format,
+                    OutputFormat = obj.obj.output_format,
+                    Example = obj.obj.example,
+                    LimitAndHint = obj.obj.limit_and_hint,
+                    TimeLimit = obj.obj.time_limit,
+                    MemoryLimit = obj.obj.memory_limit,
+                    FileIo = obj.obj.file_io,
+                    FileIoInputName = obj.obj.file_io_input_name,
+                    FileIoOutputName = obj.obj.file_io_output_name,
+                    Type = obj.obj.type,
+                    Tags = obj.obj.tags,
+                });
+                problem.IsSubmittable = true;
+            }
+            await dbContext.SaveChangesAsync();
+        }
+
+        private class ExportJson
+        {
+            public bool success { get; set; }
+            public ExportJsonObject obj { get; set; }
+        }
+
+        private class ExportJsonObject
+        {
+            public string title { get; set; }
+            public string description { get; set; }
+            public string input_format { get; set; }
+            public string output_format { get; set; }
+            public string example { get; set; }
+            public string limit_and_hint { get; set; }
+            public int time_limit { get; set; }
+            public int memory_limit { get; set; }
+            public bool file_io { get; set; }
+            public string file_io_input_name { get; set; }
+            public string file_io_output_name { get; set; }
+            public string type { get; set; }
+            public string[] tags { get; set; }
         }
     }
 }
