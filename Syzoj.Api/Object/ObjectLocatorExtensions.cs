0081aa80816335b0c1a0bfb62ce57d63b52fc645
hewenyang <hewenyang@hewenyang.cn>
2018-10-18 21:38:03 +0800

    Add default IBaseObjectLocator implementation

--- Syzoj.Api/Object/ObjectLocatorExtensions.cs
+++ Syzoj.Api/Object/ObjectLocatorExtensions.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Syzoj.Api.Object
@@ -6,9 +7,33 @@ namespace Syzoj.Api.Object
     {
         public static IServiceCollection AddBaseObjectLocator<T>(this IServiceCollection collection)
             where T : class, IBaseObjectLocator
-                => collection.AddTransient<T>();
+                => collection.AddSingleton<IBaseObjectLocator, T>();
         
         public static IServiceCollection AddObjectLocatorProvider(this IServiceCollection collection)
             => collection.AddSingleton<IObjectLocatorProvider, ObjectLocatorProvider>();
+        
+        public static IServiceCollection AddBaseObjectLocator<T>(this IServiceCollection collection, string name)
+            where T : class, IObjectLocator
+        {
+            return collection.AddSingleton<T>()
+                .AddSingleton<IBaseObjectLocator>((serviceProvider) => new BaseObjectLocator(name, serviceProvider.GetRequiredService<T>()));
+        }
+
+        private class BaseObjectLocator : IBaseObjectLocator
+        {
+            public BaseObjectLocator(string name, IObjectLocator locator)
+            {
+                this.Name = name;
+                this.locator = locator;
+            }
+
+            public string Name { get; }
+            private readonly IObjectLocator locator;
+
+            public Task<IObjectLocator> GetObjectLocator()
+            {
+                return Task.FromResult<IObjectLocator>(this.locator);
+            }
+        }
     }
 }
