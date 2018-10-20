094124734957ae5c1751fc84125ab12f247162c4
hewenyang <hewenyang@hewenyang.cn>
2018-10-21 00:17:30 +0800

    Remove logger

--- Syzoj.Api/Mvc/ObjectBinderProvider.cs
+++ Syzoj.Api/Mvc/ObjectBinderProvider.cs
@@ -10,17 +10,13 @@ namespace Syzoj.Api.Mvc
     {
         public IModelBinder GetBinder(ModelBinderProviderContext context)
         {
-            var logger = context.Services.GetRequiredService<ILogger<ObjectBinderProvider>>();
-            logger.LogDebug("GetBinder");
             if(context == null)
             {
                 throw new ArgumentNullException(nameof(context));
             }
 
-            logger.LogDebug(context.Metadata.ModelType.ToString());
             if(typeof(IObject).IsAssignableFrom(context.Metadata.ModelType))
             {
-                logger.LogDebug("Applies");
                 return new ObjectBinder();
             }
 
